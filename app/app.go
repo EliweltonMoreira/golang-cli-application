e8c8621b4308c7a08f96cf43f082aeee4d808852
EliweltonMoreira <eliwelton.moreira@outlook.com>
2022-03-23 21:58:05 -0300

    add IP search functionality

--- app/app.go
+++ app/app.go
@@ -1,11 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command Line Application"
 	app.Usage = "Get IPs and server names in the internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Get address IPs in the internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com",
+				},
+			},
+			Action: getIPs,
+		},
+	}
+
 	return app
+}
+
+func getIPs(c *cli.Context) {
+	host := c.String("host")
+
+	IPs, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, IP := range IPs {
+		fmt.Println(IP)
+	}
 }
